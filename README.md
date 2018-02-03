bf2daff79c5eed15d4c03c2d895f9a7841cf85f8
Worameth Semapat <worameth.semapat@gmail.com>
2018-02-03 23:09:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ASP.NET Core 2.1.4 + SignalR 1.0.0-alpha1-final
+
+ปัญหาของ SignalR หลักๆ คือ ไม่สามารถ disconnect user บน hub ได้
---
+++ ReportPublisher.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace client
+{
+    public class ReportsPublisher : Hub
+    {
+        //private static ConcurrentDictionary<string, string> connectingUser = new ConcurrentDictionary<string, string>();
+        private static List<string> connectingUser = new List<string>();
+        public override Task OnConnectedAsync()
+        {
+            connectingUser.Add(Context.ConnectionId);
+            Console.WriteLine(Context.ConnectionId + " is connected");
+            //AddGroup("Chen");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception ex)
+        {
+            string ConnectionId = Context.ConnectionId; 
+            connectingUser.Remove(ConnectionId);
+            Console.WriteLine(Context.ConnectionId + " is disconnected >> " + ConnectionId + " Remain: " + connectingUser.Count);
+            return base.OnDisconnectedAsync(ex);
+        }
+        public Task PublishReport(string reportName)
+        {
+            Console.WriteLine(Context.ConnectionId + " Send Message: " + reportName);
+            Console.WriteLine();
+            //return Clients.Group("Chen").InvokeAsync("OnReportPublished", reportName);
+            return Clients.All.InvokeAsync("OnReportPublished", reportName);
+        }
+
+        public Task AddGroup(string groupName)
+        {
+            return Groups.AddAsync(Context.ConnectionId, groupName);
+        }
+
+    }
+}
